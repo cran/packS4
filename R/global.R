880343e45bf6ef572d81a7d5b7f3cc3957b1b364
Christophe Genolini <genolini@u-paris10.fr>
2012-05-01 00:00:00 +0000

    version 0.9.1

--- R/global.R
+++ R/global.R
@@ -4,3 +4,4 @@ setGeneric("privateA",function(object){standardGeneric("privateA")})
 setGeneric("publicA",function(object){standardGeneric("publicA")})
 setGeneric("publicB",function(objectV,objectW){standardGeneric("publicB")})
 functionClassicA <- function(age){return(age*2)}
+
