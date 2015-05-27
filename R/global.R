2c3a7206237f28672987275ef5696d66bb5ded34
Christophe Genolini <genolini@u-paris10.fr>
2015-05-27 00:00:00 +0000

    version 0.9.3

--- R/global.R
+++ R/global.R
@@ -5,3 +5,7 @@ setGeneric("publicA",function(object){standardGeneric("publicA")})
 setGeneric("publicB",function(objectV,objectW){standardGeneric("publicB")})
 functionClassicA <- function(age){return(age*2)}
 
+#.onLoad <- function(libname,pkgname){packageStartupMessage("### [packS4] You load package <",pkgname,"> from <",libname,"> ###\n",sep="")}
+#.onAttach <- function(libname,pkgname){packageStartupMessage("### [packS4] You attach pacakge <",pkgname,"> from <",libname,"> ###\n",sep="")}
+.onUnload <- function(libpath){cat("### [packS4] You unload the pacakge ###\n",sep="")}
+
--- main.R
+++
@@ -1,8 +0,0 @@
-##################################################
-###                   main.R
-load("./data/dataAges.rda")
-source("./testsDev/testsGlobal.R")
-source("./testsDev/testsClassU.R")
-source("./testsDev/testsClassV.R")
-source("./testsDev/testsClassW.R")
-source("./testsDev/testsClassV-ClassW.R")
